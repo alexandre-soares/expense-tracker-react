2b8757e3dff4a5d3e27786ab96f97f8a8cc3618b
Alex <alx_s@hotmail.fr>
2020-07-22 16:06:36 +0300

    adding deleteTransaction function'

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Transaction = ({ transaction }) => {
+	const { deleteTransaction } = useContext(GlobalContext);
+
 	const sign = transaction.amount < 0 ? "-" : "+";
 
 	return (
@@ -10,7 +13,12 @@ export const Transaction = ({ transaction }) => {
 				{sign}
 				{Math.abs(transaction.amount)}€
 			</span>
-			<button className="delete-btn">x</button>
+			<button
+				onClick={() => deleteTransaction(transaction.id)}
+				className="delete-btn"
+			>
+				x
+			</button>
 		</li>
 	);
 };
